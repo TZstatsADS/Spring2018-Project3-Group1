fb691e378bc1a9d65baf42580b03580ea0a9d3d5
Wanting-Cui <35549396+Wanting-Cui@users.noreply.github.com>
2018-03-19 16:47:12 -0400

    cwt

--- lib/gist/togist.m
+++ lib/gist/togist.m
@@ -1,13 +1,13 @@
 datestr(now,'dd-mm-yyyy HH:MM:SS FFF')
-dic = 'C:\Users\Wanting\Documents\MATLAB\5243\project 3\images';
+dic = 'C:\Users\Wanting\Documents\MATLAB\5243\project 3\images2';
 numpic = 3000;
 
 % Parameters:
 clear param
 
 param.orientationsPerScale = [8 8 8 8];
-param.numberBlocks = 4;
-param.fc_prefilt = 4;
+param.numberBlocks = 6;
+param.fc_prefilt = 6;
 
 T = table;
 
@@ -18,5 +18,5 @@ for i = 1:numpic
     T(i,:) = table(gist);
 end
 
-writetable(T, 'gist10.csv');
+writetable(T, 'gist6.csv');
 datestr(now,'dd-mm-yyyy HH:MM:SS FFF')
